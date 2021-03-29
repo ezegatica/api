06ad368f60e0fb34c7abadeb7d44b7dcf7608a50
Eze Gatica <ezequielgatica@gmail.com>
2021-03-29 00:40:17 +0000

    Errores para que el repl corra

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/user.model")
+const User = require("../models/User.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
