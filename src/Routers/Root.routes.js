fa1058073f17abec05d5d91d02e787765dc16859
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2021-06-09 03:09:20 +0000

    Errores en repl, para repara

---
+++ src/Routers/Root.routes.js
@@ -0,0 +1,50 @@
+//Dependencias
+const { Router } = require('express');
+const rutas = Router();
+const path = require('path');
+const Link = require("../Models/Link.model")
+
+//Controller
+const Herramientas = require('../Controllers/Herramientas.controller');
+const Actions = require('../Controllers/Actions.controller');
+
+//Rutas:
+rutas.get('/watch', Herramientas.youtube);
+rutas.get('/admin', Actions.goToPanel);
+rutas.get('/panel', Actions.goToPanel);
+rutas.get("/ping", Actions.pong)
+
+
+//Archivos
+rutas.get("/", async (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, '../Pages') });
+})
+rutas.get("/fonts/:font", (req, res) => {
+    res.sendFile(req.params.font, { root: path.join(__dirname, '../assets/Fonts') });
+})
+rutas.get("/image/favicon.ico", (req, res) => {
+    res.sendFile("favicon.ico", { root: path.join(__dirname, '../assets/Images') });
+})
+rutas.get("/robots.txt", (req, res) => {
+    res.sendFile("robots.txt", { root: path.join(__dirname, '../assets/Common') });
+})
+
+//URL-Shortener
+rutas.get("/:tag", async (req, res) => {
+    try {
+        const tag = req.params.tag
+        let existingTag = await Link.findOne({ tag: tag })
+        const data = existingTag;
+        if (existingTag) {
+            await Link.updateOne({ _id: data._id }, { $inc: { conteo: 1 } })
+            res.redirect(existingTag.destino)
+        } else {
+            res.sendFile('404.html', { root: path.join(__dirname, '../Pages') });
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send();
+    }
+
+})
+module.exports = rutas;
