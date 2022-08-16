7a3bf974951dae14178e0483e48f43ddac4d9d31
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2022-08-16 00:14:50 -0300

    Testing repl.deploy

--- src/Routers/Root.routes.js
+++ src/Routers/Root.routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const rutas = Router();
 const path = require('path');
 const Link = require("../Models/Link.model")
+var {readline} = require('readline');
 
 //Controller
 const Herramientas = require('../Controllers/Herramientas.controller');
@@ -14,7 +15,7 @@ rutas.get('/admin', Actions.goToPanel);
 rutas.get('/panel', Actions.goToPanel);
 rutas.get("/ping", Actions.pong)
 
- 
+
 //Archivos
 rutas.get("/", async (req, res) => {
     res.json({
@@ -24,5 +25,25 @@ rutas.get("/", async (req, res) => {
 })
 
 rutas.get("/:username/status/:id", Herramientas.twitter)
+
+rutas.post("/refresh", async (req, res) => {
+    console.log(req.headers["signature"])
+    console.log("repl.deploy" + JSON.stringify(req.body) + req.headers["signature"])
+    let result;
+
+    var readline = require('readline');
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+    
+    rl.on('line', async function(line){
+       result = JSON.parse(line)
+    })
+
+    await res.setStatus(result.status).end(result.body)
+    console.log("repl.deploy-success")
+})
 
 module.exports = rutas;
